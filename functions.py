b3e27a9b20368603637fcc4b4e89ef8aa981458f
cneralich <corrigan@hashicorp.com>
2020-09-26 14:11:36 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -611,7 +611,7 @@ def migrate_configuration_versions(api_original, api_new, workspaces_map):
 def migrate_configuration_files(api_new, workspace_to_configuration_version_map, workspace_to_file_path_map):
     for workspace_name in workspace_to_configuration_version_map:
         # Upload the Configuration File to the New Workspace
-        # Note: The workspace_to_file_path_map must be created ahead of time with a format of {'workspace_name':'path/to/file'}
+        # Note: The workspace_to_file_path_map must be created ahead of time with a format of [{'workspace_name':'path/to/file'}]
         api_new.config_versions.upload(
             workspace_to_file_path_map[workspace_name], workspace_to_configuration_version_map[workspace_name])
     return
